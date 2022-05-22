81a645464c7ff5c2e611bba9dd4fda031334c14f
Maryna Doroshenko <verymarine@MacBook-Pro.local>
2022-05-23 01:49:13 +0300

    Project15

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -45,13 +45,14 @@ export const authorize = (email, password) => {
 }
 
 //
-export const checkToken = (token) => {
+export const checkToken = (jwt) => {
   return fetch(`${BASE_URL}/users/me`, {
     method: 'GET',
     headers: {
       "Accept": "application/json",
       "Content-Type": "application/json",
-      "Authorization" : `Bearer ${token}` 
+      authorization : jwt,
+      // "Authorization" : `Bearer ${token}` 
     }
   })
   .then(res => res.json())
