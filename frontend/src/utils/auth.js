a2f504dc930a88cba60de1d00232dc8f675b3713
Maryna Doroshenko <verymarine@MacBook-Pro.local>
2022-05-24 11:57:50 +0300

    Project15

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -8,7 +8,7 @@ export const BASE_URL = 'http://localhost:3000';
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
     method: 'POST',
-    credentials: 'include',
+    // credentials: 'include',
     headers: {
       "Accept": "application/json",
       "Content-Type": "application/json" 
@@ -28,7 +28,7 @@ export const register = (email, password) => {
 export const authorize = (email, password) => {
   return fetch(`${BASE_URL}/signin`,{
     method: 'POST',
-    credentials: 'include',
+    // credentials: 'include',
     headers: {
       "Accept": "application/json",
       "Content-Type": "application/json" ,
