e187e802e6567f80bfc5e5ab2a5b5fdfc0251731
Maryna Doroshenko <verymarine@MacBook-Pro.local>
2022-06-03 00:33:24 +0300

    Project15

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -147,7 +147,7 @@ function App() {
 
     // Отправляем запрос в API и получаем обновлённые данные карточки
     api
-      .changeLikeCardStatus(card._id, !isLiked)
+      .changeLikeCardStatus(card._id, isLiked) // HERE WAS ! before !isLiked
       .then((newCard) => {
         setCards((state) =>
           state.map((c) => (c._id === card._id ? newCard : c))
